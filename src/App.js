219a7e0062f62fd378c9c0ba5e97f7baa4467d90
Arjun <Silwal>
2021-04-18 11:03:11 +1000

    useState to track input and its history

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
-
+import React, {useState} from 'react'
 import './App.css';
 
 function App() {
+  const[text, setText] = useState('')
+  const[history, setHistory] = useState([])
   return (
     <div className="App">
+      <input placeholder="enter some text"
+      onChange ={(e) => {
+        setText(e.target.value);
+        setHistory([...history, e.target.value]);
+      }}
+
+       />
+       <h2>Your text is: {text}</h2>
+       <hr /><br />
+       <ul>
+         {history.map((r) => {
+           return <div>{r}</div>;
+         })}
+       </ul>
+
     </div>
   );
 }
